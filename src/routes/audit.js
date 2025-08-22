34aa2f8915db82e60706a31a272e05e15f7a04ce
dnyaneshwar suwarnkar <sdnyaneshwar9421@gmail.com>
2025-08-22 14:28:59 +0530

    first commit

---
+++ src/routes/audit.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import AuditLog from '../models/auditLog.js';
+import { authenticate } from '../middlewares/authMiddleware.js';
+
+const router = express.Router();
+
+// GET /api/tickets/:id/audit
+router.get('/', authenticate(), async (req, res) => {
+  console.log("Fetching audit logs for ticket:", req.params.id);
+  try {
+    const logs = await AuditLog.find({ ticketId: req.params.id }).sort({ timestamp: 1 });
+    console.log("Audit logs fetched:", logs);
+    
+    res.json(logs);
+  } catch (err) {
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+export default router;
